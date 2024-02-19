27b79a0d09a1c2ae9446a4baf0882e5b379eb7e9
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2024-02-19 09:15:57 +0000

    Index mapping implementation

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ import defaultMentionStyles from "./defaultMentionStyles";
 import defaultStyles from "./defaultStyles";
 
 function App() {
-    const [mentionInputValue, setMentionInputValue] = useState("");
+    const [markupValue, setMarkupValue] = useState("");
     const inputRef = useRef<ElementRef<"input">>(null);
-    // const [cursorPosition, setCursorPosition] = useState(0);
+    const [selectionStart, setSelectionStart] = useState(0);
 
     const tokens: SuggestionDataItem[] = [
         { display: "Subject", id: "subject" },
@@ -21,97 +21,91 @@ function App() {
         { display: "From", id: "from" },
     ];
 
-    const handleChange: OnChangeHandlerFunc = (_, newValue) => {
-        setMentionInputValue(newValue);
-
-        console.log("onChangeHandler, newValue:", newValue);
-    };
-
-    const getNearestIndexNotInToken = (
-        cursorPosition: number,
-        value: string
-    ) => {
-        // May need to add plus one here?
-        const before = value.substring(0, cursorPosition);
-        const beforeLastTokenOpening = before.lastIndexOf("{");
-        const beforeLastTokenClosing = before.lastIndexOf("}");
-        const isInsideTokenBefore =
-            (cursorPosition >= beforeLastTokenOpening &&
-                cursorPosition <= beforeLastTokenClosing) ||
-            (cursorPosition >= beforeLastTokenOpening &&
-                (beforeLastTokenClosing === -1 ||
-                    beforeLastTokenOpening > beforeLastTokenClosing));
-
-        if (isInsideTokenBefore) {
-            if (
-                beforeLastTokenClosing === -1 ||
-                beforeLastTokenOpening > beforeLastTokenClosing
-            ) {
-                const currentTokenClosing = value.indexOf("}", cursorPosition);
-
-                // Not inside a complete token
-                if (currentTokenClosing === -1) {
-                    return cursorPosition;
-                }
-
-                return currentTokenClosing + 1;
-            }
-
-            return beforeLastTokenClosing + 1;
-        }
-
-        return cursorPosition;
-    };
-
     // After a token as been added, place the cursor on the index immediately after the end of the token,
 
-    const getNearestValidIndex = (value: string, startingPosition: number) => {
-        let newIndex = startingPosition;
-        const regexWhitespace = /\s/;
+    // const getNearestValidIndex = (value: string, startingPosition: number) => {
+    //     let newIndex = startingPosition;
+    //     const regexWhitespace = /\s/;
 
-        /**
-         * Suitable characters to land on
-         * 1. Whitespace not immediately surround by non-whitespace (i.e., not "a b")
-         * 2. The end of a string, i.e., value[value.length]
-         * 3. A whitespace character that conforms to rule #1 AND is not within a token
-         * A character is within a token if its index is greater than an opening brace, but less than that brace's matching closing pair, i.e., { ^ }, where is ^ is the insertion point
-         */
-        for (let i = startingPosition; i <= value.length; ++i) {
-            const currentChar = value[i];
+    //     /**
+    //      * Suitable characters to land on
+    //      * 1. Whitespace not immediately surround by non-whitespace (i.e., not "a b")
+    //      * 2. The end of a string, i.e., value[value.length]
+    //      * 3. A whitespace character that conforms to rule #1 AND is not within a token
+    //      * A character is within a token if its index is greater than an opening brace, but less than that brace's matching closing pair, i.e., { ^ }, where is ^ is the insertion point
+    //      */
+    //     for (let i = startingPosition; i <= value.length; ++i) {
+    //         const currentChar = value[i];
 
-            // End of string, valid
-            if (currentChar === undefined) {
-                newIndex = i;
-                break;
-            }
+    //         // End of string, valid
+    //         if (currentChar === undefined) {
+    //             newIndex = i;
+    //             break;
+    //         }
 
-            // A non-whitespace character that's not the end of the string, skip
-            if (!regexWhitespace.test(currentChar)) {
-                continue;
-            }
+    //         // A non-whitespace character that's not the end of the string, skip
+    //         if (!regexWhitespace.test(currentChar)) {
+    //             continue;
+    //         }
 
-            if (value.length >= 3 && i > 0) {
-                // Whitespace between two non-whitespace characters, skip
-                if (
-                    !regexWhitespace.test(value[i - 1]) &&
-                    !regexWhitespace.test(value[i + 1])
-                ) {
-                    continue;
-                }
-            }
+    //         if (value.length >= 3 && i > 0) {
+    //             // Whitespace between two non-whitespace characters, skip
+    //             if (
+    //                 !regexWhitespace.test(value[i - 1]) &&
+    //                 !regexWhitespace.test(value[i + 1])
+    //             ) {
+    //                 continue;
+    //             }
+    //         }
 
-            const nearestTokenOpening = value.indexOf("{", i);
-            const nearestTokenClosing = value.indexOf("}", i);
-            // The token closing tag is nearer than the opening tag, so we must be inside a token; skip
-            if (nearestTokenOpening > nearestTokenClosing) {
-                continue;
-            }
+    //         const nearestTokenOpening = value.indexOf("{", i);
+    //         const nearestTokenClosing = value.indexOf("}", i);
+    //         // The token closing tag is nearer than the opening tag, so we must be inside a token; skip
+    //         if (nearestTokenOpening > nearestTokenClosing) {
+    //             continue;
+    //         }
 
-            newIndex = i;
-            break;
+    //         newIndex = i;
+    //         break;
+    //     }
+
+    //     return newIndex;
+    // };
+
+    const mapPlainTextIndex = (index: number) => {
+        const regexToken = /{.*?}/gm;
+
+        let offset = 0;
+
+        // We're at the beginning, which maps exactly
+        if (index === 0) {
+            return { index, offset };
         }
 
-        return newIndex;
+        // We're at the end, which maps exactly
+        if (index >= markupValue.length) {
+            return { index: markupValue.length, offset };
+        }
+
+        // If we're inside a tag, add one to account for the opening tag
+        // Do we need to jump to the end of the token we're inside?
+        const nextClosingTag = markupValue.indexOf("}", index);
+        const nextOpeningTag = markupValue.indexOf("{", index);
+        if (nextClosingTag !== -1 && nextClosingTag < nextOpeningTag) {
+            offset += 1;
+            index += offset;
+        }
+
+        // Offset by the number of complete tags before the index
+        const before = markupValue.slice(0, index);
+        offset += (before.match(regexToken)?.length ?? 0) * 2;
+
+        index += offset;
+
+        return {
+            index: Math.min(index, markupValue.length),
+            offset,
+        };
     };
 
     const handleAddToken = () => {
@@ -120,61 +114,60 @@ function App() {
             return;
         }
 
-        const cursorPosition = getNearestValidIndex(
-            mentionInputValue,
-            inputElement.selectionStart ?? 0
-        );
+        const { index: markupIndex, offset } =
+            mapPlainTextIndex(selectionStart);
 
-        // let cursorPosition = getNearestIndexNotInToken(
-        //     inputElement.selectionStart ?? 0,
-        //     mentionInputValue
+        // const cursorPosition = getNearestValidIndex(
+        //     markupValue,
+        //     inputElement.selectionStart ?? 0
         // );
 
-        // const regexWhitespace = /\s/;
-
-        // for (let i = cursorPosition; i < mentionInputValue.length; ++i) {
-        //     const charUnderCursor = mentionInputValue[i];
-        //     if (!!charUnderCursor && !regexWhitespace.test(charUnderCursor)) {
-        //         cursorPosition += 1;
-        //     }
-        //     // if we're in between two tokens
-        //     if (
-        //         !regexWhitespace.test(mentionInputValue[i - 1]) &&
-        //         !regexWhitespace.test(mentionInputValue[i + 1])
-        //     ) {
-        //     }
-        // }
-
         // Get text before and after cursor
-        const before = mentionInputValue.substring(0, cursorPosition);
-        const after = mentionInputValue.substring(
-            cursorPosition,
-            mentionInputValue.length
-        );
+        const before = markupValue.substring(0, markupIndex);
+        const after = markupValue.substring(markupIndex, markupValue.length);
 
         // Set the new input value, insert the @ at the location of the cursor
         const newValue = before + "@" + after;
 
-        setMentionInputValue(newValue);
+        setMarkupValue(newValue);
 
-        // // requestAnimationFrame(() => {
-        // const newCursorLocation = newValue.indexOf("@", cursorPosition);
-        // inputElement.setSelectionRange(newCursorLocation, newCursorLocation);
-        // // });
+        requestAnimationFrame(() => {
+            // Need to map back to plain text index to set cursor location
+            const newCursorLocation = markupIndex - offset + 1;
+            inputElement.setSelectionRange(
+                newCursorLocation,
+                newCursorLocation
+            );
+            // inputElement.dispatchEvent(new Event("select"));
+        });
 
         // Focus the element
         inputElement.focus();
     };
 
-    const handleAdd = () => {
-        const currentSelection = inputRef.current?.selectionStart ?? 0;
+    const handleChange: OnChangeHandlerFunc = (
+        _,
+        markupValue,
+        plainTextValue
+    ) => {
+        setMarkupValue(markupValue);
 
-        // setCursorPosition(currentSelection);
+        console.log("onChangeHandler, markupValue:", markupValue);
+        console.log("onChangeHandler, plainTextValue:", plainTextValue);
 
-        console.log("handleAdd, currentSelection:", currentSelection);
+        console.log("onChangeHandler, markupValue.length:", markupValue.length);
+        console.log(
+            "onChangeHandler, plainTextValue.length:",
+            plainTextValue.length
+        );
     };
 
-    console.log("Value length", mentionInputValue.length);
+    const handleSelect = () => {
+        const selectionStart = inputRef.current?.selectionStart ?? 0;
+
+        setSelectionStart(selectionStart);
+        console.log("selectionStart:", selectionStart);
+    };
 
     return (
         <div className="container">
@@ -183,16 +176,16 @@ function App() {
                 Mentions Input
                 <MentionsInput
                     onChange={handleChange}
-                    value={mentionInputValue}
+                    value={markupValue}
                     inputRef={inputRef}
                     style={defaultStyles}
+                    onSelect={handleSelect}
                 >
                     <Mention
                         trigger="@"
                         data={tokens}
                         markup="{__display__}"
                         style={defaultMentionStyles}
-                        onAdd={handleAdd}
                     />
                 </MentionsInput>
             </label>
